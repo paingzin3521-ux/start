3db0af64d6f1243fe07feff299d1f61764b14d3f
paingzin3521-ux <paingzin3521@gmail.com>
2026-07-22 14:45:36 +0000

    add star shortcut + install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/data/data/com.termux/files/usr/bin/bash
+cp star $PREFIX/bin/star
+chmod +x $PREFIX/bin/star
+echo ""
+echo "✅ Done! Now type: star"
+echo ""
---
+++ star
@@ -0,0 +1,3 @@
+#!/data/data/com.termux/files/usr/bin/bash
+cd ~/start
+python run.py
